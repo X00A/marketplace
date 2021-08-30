9dcda80d2f08e7728f59df6792281a75c0105ac7
Secareanu Alexandru <poly.vinyl00@gmail.com>
2021-08-30 18:11:22 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Products from "./view/Products";
+import Home from "./view/Home";
+import Navigation from "./components/Navigation/Navigation";
+
 function App() {
-  return <div>Hello Marketplace!</div>;
+  return (
+    <>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='*'>404:Page Not Found</Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
